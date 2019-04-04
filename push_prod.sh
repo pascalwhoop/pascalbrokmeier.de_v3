94bf13070075be48e1500b590b9de92ffc747ec3
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2019-04-04 16:33:34 +0200

    prod build

--- push_prod.sh
+++ push_prod.sh
@@ -1,5 +1,5 @@
-cd ..
-cd ../pascalbrokmeier.de_build/ 
+#!/usr/bin/fish
+cd /home/pascalwhoop/Documents/Code/website/pascalbrokmeier.de_build
 git add ./
 git commit -m "prod build"
-git push origin master:gh-pages
+git push -f origin master:gh-pages

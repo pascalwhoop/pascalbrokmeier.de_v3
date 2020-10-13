96a9a04ac0fd40e4a2fdf29375eef41bdb83930f
Pascal Brokmeier <git@pascalbrokmeier.de>
2020-10-13 17:54:57 +0200

    remove GA to avoid any issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,29 +134,29 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-55919824-7',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        // pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
-        // sampleRate: 5,
-        // siteSpeedSampleRate: 10,
-        // cookieDomain: "example.com",
-      },
-    },
+    //{
+    //  resolve: `gatsby-plugin-google-analytics`,
+    //  options: {
+    //    trackingId: 'UA-55919824-7',
+    //    // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //    head: false,
+    //    // Setting this parameter is optional
+    //    anonymize: true,
+    //    // Setting this parameter is also optional
+    //    respectDNT: true,
+    //    // Delays sending pageview hits on route update (in milliseconds)
+    //    // pageTransitionDelay: 0,
+    //    // Enables Google Optimize using your container Id
+    //    // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+    //    // Enables Google Optimize Experiment ID
+    //    // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+    //    // Set Variation ID. 0 for original 1,2,3....
+    //    // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+    //    // Any additional optional fields
+    //    // sampleRate: 5,
+    //    // siteSpeedSampleRate: 10,
+    //    // cookieDomain: "example.com",
+    //  },
+    //},
   ],
 }
